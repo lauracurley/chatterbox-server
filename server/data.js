a0cb4b709ae23e1f851519b819806bc4ea7a053f
lauracurley <info@lauracurley.com>
2016-03-21 17:00:53 -0700

    Add sample data

---
+++ server/data.js
@@ -0,0 +1,15 @@
+var messages = {
+  results: []
+};
+
+var sample = {};
+sample.createdAt = '2016-03-16T04:48:19.161Z';
+sample.objectId = 'qq23jc4wI2';
+sample.roomname = 'happy_pandaz_only';
+sample.text = '</script>console.log("yoyoyoyo");';
+sample.updatedAt = '2016-03-16T04:48:19.161Z';
+sample.username = 'Testing stilll';
+
+messages.results.push(sample);
+
+exports.messages = messages;
